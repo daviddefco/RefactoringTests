0a9df58b317e497994c8cc992734dde43c3e176c
daviddefco <david.de.fco@gmail.com>
2013-12-11 12:03:04 +0100

    Replace Conditional with Polymorphism -> frequent renter points

--- refactoring/src/main/java/com/daviddefrancisco/movies/Price.java
+++ refactoring/src/main/java/com/daviddefrancisco/movies/Price.java
@@ -2,5 +2,8 @@ package com.daviddefrancisco.movies;
 
 public abstract class Price {
 	abstract int getPriceCode();	
-	abstract double getCharge(int daysRented);
+	abstract double getCharge(int daysRented);	
+	int getFrequentRenterPoints(int daysRented) {
+		return 1;
+	}
 }	
