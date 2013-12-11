fab3e3037445a83ed47b60da9a30fcc5342312c8
daviddefco <david.de.fco@gmail.com>
2013-12-11 10:06:08 +0100

    Extract method getFrequentRenterPoints

--- refactoring/src/main/java/com/daviddefrancisco/movies/Rental.java
+++ refactoring/src/main/java/com/daviddefrancisco/movies/Rental.java
@@ -35,5 +35,14 @@ class Rental {
 			break;
 		}
 		return result;
-	}	
+	}
+	
+	int getFrequentRenterPoints() {
+		// add bonus for a two day new release rental
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE)
+				&& getDaysRented() > 1)
+			return 2;
+		else
+			return 1;
+	}
 }
